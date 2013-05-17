751d634e217d6eb6f5a713994863606b739d6b3f
Makoto Isawa <makoto.isawa@gmail.com>
2013-05-17 23:03:12 +0900

    Added bootstrap, and applied layout to the page

---
+++ diary-makorin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiaryMakorin::Application.config.secret_token = '60a352176f111e56eb32f26c0a4f62d224e5e6ec964bac1c4f52f84b9af7934c8bf7ec48921b50d8cc2a9cf83b7d0105c9386c19474098f4410561660f3927d5'
